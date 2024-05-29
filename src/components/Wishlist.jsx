0c6f69f37fc22a8f742c9a7e74bb17040c7f22c9
Scharly <scharly.opitz@gmail.com>
2024-05-29 13:20:00 +0200

    Apparition du container à partir du moment où un item est présent

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -7,26 +7,28 @@ export default function Wishlist({ list, setList }) {
   }
 
   return (
-    <div className="wishlistContainer">
-      {list.map((obj, idx) => {
-        return (
-          <div className="wishlistRow" key={idx} data-id={idx}>
-            <p>{obj.titleValue}</p>
-            <a href={obj.linkValue} target="_blank">
-              clique
-            </a>
-            <p>{obj.descriptionValue}</p>
-            <button
-              className="delete"
-              onClick={() => {
-                deleteBook(obj.titleValue);
-              }}
-            >
-              L
-            </button>
-          </div>
-        );
-      })}
-    </div>
+    list.length > 0 && (
+      <div className="wishlistContainer">
+        {list.map((obj, idx) => {
+          return (
+            <div className="wishlistRow" key={idx}>
+              <p>{obj.titleValue}</p>
+              <a href={obj.linkValue} target="_blank">
+                clique
+              </a>
+              <p>{obj.descriptionValue}</p>
+              <button
+                className="delete"
+                onClick={() => {
+                  deleteBook(obj.titleValue);
+                }}
+              >
+                L
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    )
   );
 }
