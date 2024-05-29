1f60738173461a4d63273225b9e7d347daf77566
Scharly <scharly.opitz@gmail.com>
2024-05-29 14:34:22 +0200

    Ajout des packages fontAwesome pour l'ajout des icons

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+
 export default function Wishlist({ list, setList }) {
   function deleteBook(titleValue) {
     const filteredList = list.filter((item) => {
@@ -14,7 +17,10 @@ export default function Wishlist({ list, setList }) {
             <div className="wishlistRow" key={idx}>
               <p>{obj.titleValue}</p>
               <a href={obj.linkValue} target="_blank">
-                clique
+                <FontAwesomeIcon
+                  icon={faArrowUpRightFromSquare}
+                  style={{ color: "#58b0f3" }}
+                />
               </a>
               <p>{obj.descriptionValue}</p>
               <span
