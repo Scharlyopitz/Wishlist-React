3b7538413efe252780e696ff4c5e9b24ccf8d7d3
Scharly <scharly.opitz@gmail.com>
2024-05-29 13:02:45 +0200

    Header mise au propre

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 export default function Header() {
   return (
-    <div>
-      <h1>
-        My<h1>Wish</h1>list
-      </h1>
-    </div>
+    <header>
+      <span>My</span>
+      <span>Wish</span>
+      <span>List</span>
+    </header>
   );
 }
